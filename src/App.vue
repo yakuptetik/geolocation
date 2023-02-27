5bd1885180f123b1c1e3547c813ddf342e5f42a1
Yakup <yakuptetik13@gmail.com>
2023-02-27 13:55:11 +0300

    sqwde

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,31 @@
 <script setup>
-import { onMounted } from 'vue';
+import { onMounted, ref } from 'vue';
 import { useLocationStore } from './store/location'
 
 const useLocal = useLocationStore()
+const isLoading = ref(true);
 
 onMounted(() => {
-  useLocal.fetchLocation();
+  useLocal.fetchLocation()
+  .then(() => {
+    isLoading.value = false;
+  });
 })
 
 </script>
 
 <template>
   <div  class="bg-blue-300 w-screen h-screen p-5 flex items-center justify-center">
-    <div class="">
-    <div class="flex items-center justify-start pt-8 text-white text-4xl">
-      Your IP: {{ useLocal.locations.ip_address }}
-    </div>
-    <div class="flex items-center justify-start pt-8 text-white text-4xl">
-      Your Location: {{ useLocal.locations.region }}/{{ useLocal.locations.country }}
-    </div>
-    <div class="flex items-center justify-start pt-8 text-white text-4xl">
-      Your Flag: {{ useLocal.locations.flag.emoji }}
-    </div>      
+    <div class="" v-if="!isLoading">
+      <div class="flex items-center justify-start pt-8 text-white text-4xl">
+        Your IP: {{ useLocal.location.ip_address }}
+      </div>
+      <div class="flex items-center justify-start pt-8 text-white text-4xl">
+        Your Location: {{ useLocal.location.region }}/{{ useLocal.location.country }}
+      </div>
+      <div class="flex items-center justify-start pt-8 text-white text-4xl">
+        Your Flag: {{ useLocal.location.flag.emoji }}
+      </div>      
     </div>
 
   </div>
--- src/store/location.js
+++ src/store/location.js
@@ -3,23 +3,22 @@ import { defineStore } from 'pinia';
 import axios from 'axios'
 
 export const useLocationStore = defineStore('location', () => {
-  const locations = ref()
+
+  const location = ref({})
 
   function fetchLocation() {
     return new Promise((resolve, reject) => {
       try {
         axios.get('https://ipgeolocation.abstractapi.com/v1/?api_key=2b1dae796fba4614bface662a27962d8')
           .then((response) => {
-            locations.value = response.data;
+            location.value = response.data;
             console.log(response.data)
             resolve();
-
           });
       }	catch (err) {
-        reject(err);
       }
     });
 }
 
-  return { locations, fetchLocation}
+  return {location,  fetchLocation}
 })
